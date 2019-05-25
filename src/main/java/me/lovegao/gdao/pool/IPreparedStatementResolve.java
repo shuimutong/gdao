47fb07cbb623f3aca48d6d14452cc15abb67c2b7
simple <simpleha@qq.com>
2019-05-25 12:00:21 +0800

    还原版本

--- src/main/java/me/lovegao/gdao/pool/IPreparedStatementResolve.java
+++
@@ -1,7 +0,0 @@
-package me.lovegao.gdao.pool;
-
-import java.sql.PreparedStatement;
-
-public interface IPreparedStatementResolve<T> {
-	T solvePreparedStatement(PreparedStatement ps) throws Exception;
-}
